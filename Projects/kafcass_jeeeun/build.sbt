12b0259ebea0714a317631bf52e2c43bddef482c
Al Sharma <kunalnano@icloud.com>
2019-10-03 21:37:25 -0400

    Kafka to cassandra script, hybrid!

--- Projects/kafcass_jeeeun/build.sbt
+++ Projects/kafcass_jeeeun/build.sbt
@@ -4,14 +4,12 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.1"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
-libraryDependencies += "org.apace.spark" %% "spark-streaming" % "2.4.1"
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.3.4"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-twitter" % "1.6.0"
 libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka" % "1.6.0"
-libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.3"
-libraryDependencies += "org.apache.spark" %% "spark-streaming-twitter" % "1.6.3"
-
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.3.1"
+libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "3.5.2" artifacts (Artifact("stanford-corenlp", "models"), Artifact("stanford-corenlp"))
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.3.0"
 libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.1"
-
-libraryDependencies += "org.twitter4j" % "twitter4j-core" % "4.0.6"
--- Projects/kafcass_jeeeun/src/main/scala/kacasstweet.scala
+++ Projects/kafcass_jeeeun/src/main/scala/kacasstweet.scala
@@ -1,10 +1,90 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.{seconds, StreamingContext}
+
+import java.util.Date
+
+import com.datastax.driver.core.Cluster
+import com.datastax.spark.connector._
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object kacasstweet {
 
   def main(args: Array[String]): Unit = {
 
+    //Start a new Spark Configuration
     val conf = new SparkConf()
+      .setAppName("Al")
+      .setMaster("[local(*)]")
+
+    // Link SparkStreamingContext and SparkConf with new SparkContext to connect with Spark Cluster
+    val ssc = new StreamingContext(conf, Seconds(5))
+    val sc = ssc.sparkContext
+
+    //Set LogLevel to "WARM" Anything that can potentially cause application oddities, with automatic recovery
+    //Set LogLevel to "Error" Any error which is fatal to the operation, but not the service or application
+    sc.setLogLevel("WARN")
+
+    //Set Topic
+    val topic = List("Israel").toSet
+
+    //Define and call Kafka Parameters to connect to existing Kakfa instance
+    //Since kafka is running local -> bootstrap.servers -> localhost:9092
+    val kafkaParams = Map(
+    "bootstrap.servers"->"localhost:9092",
+
+    // Deserialize a String buffer into an object, given the class of the object.
+    "key.deserializer"->classOf[StringDeserializer],
+    "value.deserializer"->classOf[StringDeserializer],
+
+    //Assign a group ID for kafka stream consumers
+    "group.id"->"spark-stream-twitter"
+    // earliest: automatically reset the offset to the earliest offset
+    "auto.offset.reset"->"earliest"
+    )
+
+    // Connect Spark to Cassandra
+    val cassandra_host = conf.get("spark.cassandra.connection.host", "localhost");
+
+    val cluster = Cluster.builder().addContactPoint(cassandra_host).build()
+    val session = cluster.connect()
+    session.execute("CREATE KEYSPACE IF NOT EXISTS twitter WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+    session.execute("CREATE TABLE IF NOT EXISTS twitter.tweet_stream (id int, name text, text text, word text, ts timestamp, count int, PRIMARY KEY(word, ts)) ")
+    //session.execute("TRUNCATE Tweet")
+
+    // Set kafka parameters for streaming context
+    val line = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topic, kafkaParams)
+    )
+
+    val streaming_tweets = line.map(_.value) // split tweet into lines
+      .flatMap(_.split(" ")) // split tweet into words
+      .flatMap(_.split(","))
+      .filter(w=>w.length()>0) // remove any empty words caused by double spaces
+      .map(w=>(w, 1L)).reduceByKey(_+_) // count by word line for each incoming RDD
+      .map({case (w,c) => (w,new Date().getTime,c)}) // add the current timestamp saveToCassandra("Tweet,count")})
+
+    streaming_tweets.print()
+
+    streaming_tweets.foreachRDD(rdd => {
+      println("------------------------------------------")
+      if (rdd.collect().size == 0)
+        println("NOTHING YET")
+      else{
+        println("size ="+rdd.collect().size())
+      }
+      rdd.saveToCassandra("twitter", "tweet_stream")
+    })
+    ssc.start() // start the streaming context
+    ssc.awaitTermination() // block while the context is running (until it's stopped by the timer)
+    ssc.stop()
+
+    // Get results using Spark SQL
+    val rdd1 = sc.cassandraTable("twitter", "tweet_stream")
+    rdd1.take(100).foreach(println)
+    sc.stop()
   }
 }
